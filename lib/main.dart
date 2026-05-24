74dabd51c0a993c89a9fec9df0deeed66aa836b4
bobymoz <itanhagira@gmail.com>
2026-05-25 00:44:11 +0200

    Replace lucide_icons import with lucide_icons_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lucide_icons/lucide_icons.dart';
-
-// ==========================================
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 // 1. INICIALIZAÇÃO E CONEXÃO COM SUPABASE
 // ==========================================
 Future<void> main() async {
