182fcaeb15ea3c75fe616ff2333978ad4275698f
bobymoz <itanhagira@gmail.com>
2026-05-24 18:13:24 +0200

    Change Supabase anonKey in main.dart
    Updated Supabase anonKey with a new value.

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ Future<void> main() async {
   await Supabase.initialize(
     // TODO: Replace credentials with your own
     url: 'SUPABASE_URL',
-    anonKey: 'SUPABASE_ANON_KEY',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnZWRkbnBjY2tvcWlsY3FzdG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MjU5MjksImV4cCI6MjA5NTIwMTkyOX0.827gFhTAqkSF1OSuysXVr_dACyTPekYwHSqlGHcj9aQ',
   );
   runApp(const MyApp());
 }
