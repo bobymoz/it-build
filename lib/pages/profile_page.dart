79724b1e5f3711c21d5396ed0da4e1fd465ef9c0
bobymoz <itanhagira@gmail.com>
2026-05-26 00:34:14 +0200

    Update profile_page.dart

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,1 +1,7 @@
-
+import 'package:flutter/material.dart';
+class ProfilePage extends StatelessWidget {
+  final String userId;
+  const ProfilePage({super.key, required this.userId});
+  static Route<void> route(String id) => MaterialPageRoute(builder: (_) => ProfilePage(userId: id));
+  @override Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: const Text('Perfil')), body: const Center(child: Text('A carregar perfil...')));
+}
